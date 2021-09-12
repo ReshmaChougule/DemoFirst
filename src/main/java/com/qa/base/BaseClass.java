eab4c9a3ec387de0159f9ed5e37af01d72ace8e4
ReshmaChougule <reshmachougule111@gmail.com>
2021-09-12 14:22:53 +0530

    Second Demo

---
+++ src/main/java/com/qa/base/BaseClass.java
@@ -0,0 +1,9 @@
+package com.qa.base;
+
+public class BaseClass {
+	public void m1() {
+	System.out.println("hello");
+
+	}
+
+}
---
+++ src/test/java/com/qa/testcases/HomeTest.java
@@ -0,0 +1,9 @@
+package com.qa.testcases;
+
+public class HomeTest {
+	public void m2() {
+	System.out.println("I am first");
+
+	}
+
+}
